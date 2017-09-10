0dd57d06b4c1fb431b3b692a1ca617ab286064ae
mplibunao <markpaololibunao@gmail.com>
2017-09-10 21:25:45 +0800

    Added cors middleware and fixed json response for root route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const app =  express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
@@ -12,6 +14,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 // App Setup
 
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
